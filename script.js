334cd6e822199f79ce8c58bf5e039c82d9b50740
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-06-13 23:24:29 -0700

    return password

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -16,10 +13,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-
+// Global variables for state
 let passMin = 8;
 let passMax = 256;
 let allowLowerCase = false;
@@ -28,41 +22,58 @@ let allowNumbers = false;
 let allowSpecial = false;
 let passlength = 0;
 
-let lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
-let upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let numbersChar = "0123456789";
-let specialChar = `!"#$%&'()*+,-./:;<=>?@[\\\]^_\`\{|}~"`;
+// Potential characters
+let lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let numbersChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"];
 
 
 function generatePassword() {
-  let possibleChars = "";
+
+  let possibleChars = [];
+  let pass = [];
   findLengthMin();
   findLengthMax();
   validateCharacters();
 
+  // Find length of password
   passlength = Math.floor(Math.random() * (passMax - passMin) + passMin);
+
+  // Creating array of possible characters
   if (allowLowerCase) {
-    possibleChars += lowerCaseChar;
+    possibleChars = [...possibleChars, ...lowerCaseChar];
 
   }
 
   if (allowUpperCase) {
-    possibleChars += upperCaseChar;
+    possibleChars = [...possibleChars, ...upperCaseChar];
   }
 
   if (allowNumbers) {
-    possibleChars += numbersChar;
+    possibleChars = [...possibleChars, ...numbersChar];
   }
 
   if (allowSpecial) {
-    possibleChars += specialChar;
+    possibleChars = [...possibleChars, ...specialChar];
   }
 
+  // Loop through the length of password to randomly add character from list.
+  for (let i = 0; i <= passlength; i++) {
+    let randomChar = possibleChars[Math.floor(Math.random() * possibleChars.length)];
+    pass.push(randomChar);
+  }
+
+  // Turn array to string
+  return pass.join("");
 }
 
 function findLengthMin() {
+  // prompt user to enter
   let passwordMin = window.prompt("What is the minimum password length?");
   passwordMin = parseInt(passwordMin);
+
+  // validate minimum
   if (!passMin || typeof passwordMin !== "number") {
     window.alert("Please enter a valid number.");
     findLengthMin()
@@ -70,13 +81,18 @@ function findLengthMin() {
     window.alert("Password needs to be a minimum of 8 characters. Try again.");
     findLengthMin()
   }
+
+  //update global
   passMin = passwordMin;
 
 }
 
 function findLengthMax() {
+  // prompt user to enter
   let passwordMax = window.prompt("What is the maximum password length?");
   passwordMax = parseInt(passwordMax);
+
+  //validate max
   if (!passwordMax || typeof passwordMax !== "number") {
     window.alert("Please enter a valid number.");
     findLengthMax();
@@ -84,14 +100,19 @@ function findLengthMax() {
     window.alert("The password maximum length is 256. Try again.");
     findLengthMax();
   }
+  //update global
   passMax = passwordMax;
 
 }
 
 function checkLowerCase() {
+  // prompt user to enter
   let lowerCase = window.prompt('Do you want to include lowercase? Type "YES" or "NO"');
   lowerCase = lowerCase.toUpperCase();
+
+  //validate yes or no
   if (lowerCase === "YES") {
+    //update global
     allowLowerCase = true;
   } else if (lowerCase === "NO") {
     return;
@@ -103,9 +124,13 @@ function checkLowerCase() {
 
 
 function checkUpperCase() {
+  // prompt user to enter
   let upperCase = window.prompt('Do you want to include uppercase? Type "YES" or "NO"');
   upperCase = upperCase.toUpperCase();
+
+  //validate yes or no
   if (upperCase === "YES") {
+    //update global
     allowUpperCase = true;
   } else if (upperCase === "NO") {
     return;
@@ -116,9 +141,12 @@ function checkUpperCase() {
 }
 
 function checkNumbers() {
+  // prompt user to enter
   let number = window.prompt('Do you want to include numbers? Type "YES" or "NO"');
   number = number.toUpperCase();
+  //validate yes or no
   if (number === "YES") {
+    //update global
     allowNumbers = true;
   } else if (number === "NO") {
     return;
@@ -129,9 +157,12 @@ function checkNumbers() {
 }
 
 function checkSpecial() {
+  // prompt user to enter
   let special = window.prompt('Do you want to include special characters? Type "YES" or "NO"');
   special = special.toUpperCase();
+  //validate yes or no
   if (special === "YES") {
+    //update global
     allowSpecial = true;
   } else if (special === "NO") {
     return;
@@ -143,19 +174,16 @@ function checkSpecial() {
 }
 
 function validateCharacters() {
+  // Ask for potential characters
   checkLowerCase();
   checkUpperCase();
   checkNumbers();
   checkSpecial();
+
+  // validate that at least one type of character is selected.
   if (!allowLowerCase && !allowUpperCase && !allowNumbers && !allowSpecial) {
     window.alert(`At least one character type needs to be selected. Try again`);
     validateCharacters();
   };
 }
 
-function findLength(min, max) {
-  console.log(min)
-  console.log(max)
-  passlength = Math.floor(Math.random() * (max - min) + min);
-  console.log(passlength)
-}
