5b2ac046c9041b20eb19b555997244f3765d7145
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-06-13 22:11:48 -0700

    add validateCharacters function

--- script.js
+++ script.js
@@ -22,10 +22,16 @@ generateBtn.addEventListener("click", writePassword);
 
 let passMin = 8;
 let passMax = 256;
+let allowLowerCase = false;
+let allowUpperCase = false;
+let allowNumbers = false;
+let allowSpecial = false;
 
 function generatePassword() {
   findLengthMin();
   findLengthMax();
+  validateCharacters();
+
 
 
   // var passwordMax = window.prompt("What is the maximum password length?");
@@ -74,3 +80,69 @@ function findLengthMax() {
   passMax = passMax;
 
 }
+
+function checkLowerCase() {
+  let lowerCase = window.prompt('Do you want to include lowercase? Type "YES" or "NO"');
+  lowerCase = lowerCase.toUpperCase();
+  if (lowerCase === "YES") {
+    allowLowerCase = true;
+  } else if (lowerCase === "NO") {
+    return;
+  } else {
+    window.alert(`Please type "YES" or "NO".`);
+    checkLowerCase();
+  }
+}
+
+
+function checkUpperCase() {
+  let upperCase = window.prompt('Do you want to include uppercase? Type "YES" or "NO"');
+  upperCase = upperCase.toUpperCase();
+  if (upperCase === "YES") {
+    allowUpperCase = true;
+  } else if (upperCase === "NO") {
+    return;
+  } else {
+    window.alert(`Please type "YES" or "NO".`);
+    checkUpperCase();
+  }
+}
+
+function checkNumbers() {
+  let number = window.prompt('Do you want to include numbers? Type "YES" or "NO"');
+  number = number.toUpperCase();
+  if (number === "YES") {
+    allowNumbers = true;
+  } else if (number === "NO") {
+    return;
+  } else {
+    window.alert(`Please type "YES" or "NO".`);
+    checkNumbers();
+  }
+}
+
+function checkSpecial() {
+  let special = window.prompt('Do you want to include special characters? Type "YES" or "NO"');
+  special = special.toUpperCase();
+  if (special === "YES") {
+    allowSpecial = true;
+  } else if (special === "NO") {
+    return;
+  } else {
+    window.alert(`Please type "YES" or "NO".`);
+    checkSpecial();
+  }
+
+}
+
+function validateCharacters() {
+  checkLowerCase();
+  checkUpperCase();
+  checkNumbers();
+  checkSpecial();
+  if (!allowLowerCase && !allowUpperCase && !allowNumbers && !allowSpecial) {
+    window.alert(`At least one character type needs to be selected. Try again`);
+    validateCharacters();
+  };
+}
+
